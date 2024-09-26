480db0ab48690039aa07f8cd99c36c4ac3624913
Jose Mayo <josemayo3@gmail.com>
2024-09-26 11:32:55 -0400

    push tasks

--- TypeScript/task_0/js/main.ts
+++ TypeScript/task_0/js/main.ts
@@ -1,22 +1,47 @@
 interface Student {
-    firstName: string
-    lastName: string
-    age: number
-    location: string
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
 }
 
-const student1: Student = {
+let student1: Student = {
     firstName: 'John',
     lastName: 'Doe',
     age: 20,
     location: 'New York',
 };
 
-const student2: Student = {
+let student2: Student = {
     firstName: 'Jane',
     lastName: 'Smith',
     age: 25,
     location: 'Los Angeles',
 };
 
-const studentsList: Array<Student> = [student1, student2];
+let studentsList: Student[] = [student1, student2];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const table = document.createElement('table');
+
+    // Create table rows
+    const headerRow = table.insertRow();
+    const headerNames = ["First Name", "Location"];
+    headerNames.forEach((name) => {
+        const headerCell = document.createElement('th');
+        headerCell.textContent = name;
+        headerRow.appendChild(headerCell);
+    });
+
+    // Create row for students in the students list
+    studentsList.forEach((student) => {
+        const row = table.insertRow();
+        const firstNameCell = row.insertCell();
+        firstNameCell.textContent = student.firstName;
+        const locationCell = row.insertCell();
+        locationCell.textContent = student.location;
+    });
+
+    // Append table to the document body
+    document.body.appendChild(table);
+});
---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,18 @@
+interface Teacher {
+    firstName: string,
+    lastName: string,
+    fullTimeEmployee: boolean,
+    yearsOfExperience: number(optional)
+    location: string,
+    contract: boolean
+}
+
+const teacher1: Teacher = {
+    firstName: 'John',
+    lastName: 'Doe',
+    fullTimeEmployee: true,
+    yearsOfExperience: 10,
+    location: 'New York',
+    contract: true,
+};
+
